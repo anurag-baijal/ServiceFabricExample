dd7eca930d6641093674ec43ecf3ebf20c6ae027
Anurag Baijal <2403@xceedance.com>
2020-01-10 10:53:40 +0530

    Add project files.

---
+++ MyServiceFabricAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyServiceFabricAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
